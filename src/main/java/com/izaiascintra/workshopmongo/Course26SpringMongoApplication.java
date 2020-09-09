11686d8618496f571e9371ee3e9edcf9985ca754
Izaias Cintra <izaiasacintra@gmail.com>
2020-09-09 18:50:08 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/izaiascintra/workshopmongo/Course26SpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.izaiascintra.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course26SpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course26SpringMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izaiascintra/workshopmongo/Course26SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izaiascintra.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course26SpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
